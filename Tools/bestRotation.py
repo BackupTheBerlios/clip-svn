867dcec25887f0e00317446bcb2ae7b0d144922e
schumann <schumann@2a80fe08-8f50-407d-9771-5f6270efea71>
2008-05-30 09:20:10 +0000

    SolutionFinder hinzu

--- Tools/bestRotation.py
+++ Tools/bestRotation.py
@@ -1,4 +1,7 @@
 import sys
+import scipy
+import scipy.linalg
+
 sys.path.append('..')
 
 from ToolBox import Mat3D, Vec3D
@@ -13,30 +16,69 @@ def rV(d):
 def rVn(d=1):
     return rV(d).normalized()
 
-C=[]
-for n in range(30):
-    C.append(rVn())
+    
+def outer(v):
+  M=Mat3D()
+  for i in range(3):
+    for j in range(3):
+      M[i,j]=v[i]*v[j]
+  return(M)
 
+for s in range(10):
+    C=[]
+    for n in range(10):
+        C.append(rVn())
+    RMat=Mat3D(rVn(),  3*random())
+    V=[(RMat*x+rV(0.1)).normalized() for x in C]
+    #V=[RMat*x for x in C]
 
-RMat=Mat3D(rVn(),  random())
-
-V=[(RMat*x+rV(0.01)).normalized() for x in C]
-
-#V[4]=rVn()
-
-for i in range(1000):
-    M1=Mat3D((0, 0, 0, 0, 0, 0, 0, 0, 0))
-    M2=Mat3D((0, 0, 0, 0, 0, 0, 0, 0, 0))
-
+    M=scipy.zeros((3, 3))
     for c, v in zip(C, V):
         for i in range(3):
             for j in range(3):
-                M1[i, j]+=c[i]*c[j]
-                M2[i, j]+=v[i]*c[j]
-            
-    M=M2*M1.inverse()
-    score=[]
-    for i in range(10):
-        
-        score.append(sum([(v-T*c).norm() for c, v in zip(C, V)]))
-        M=M.orthogonalize()
+                M[i, j]+=c[i]*v[j]
+                
+    U, s, V=scipy.linalg.svd(M)
+    M=Mat3D(M)
+    U=Mat3D(U)
+    V=Mat3D(V)
+    S=Mat3D()
+    for i in range(3):
+        S[i, i]=s[i]
+    
+    sco=(RMat-V*U).sqSum()
+    print (M-V*S*U).sqSum(), sco
+    
+    
+
+def UToUpper(M):
+    v1=M*Vec3D(1,0,0)
+    u1=v1-Vec3D(1,0,0)*v1.norm()
+    u1.normalize()
+    R1=Mat3D()-outer(u1)*2
+
+    v2=R1*M*Vec3D(0,1,0)
+    v2[0]=0.0
+    u2=v2-Vec3D(0,1,0)*v2.norm()
+    u2.normalize()
+    R2=Mat3D()-outer(u2)*2
+    return R2*R1
+    
+def LRToBiDiag(M):
+    v1=M*Vec3D(1,0,0)
+    u1=(v1-Vec3D(1,0,0)*v1.norm()).normalized()
+    L1=Mat3D()-outer(u1)*2
+    
+    v2=(L1*M).transpose()*Vec3D(1,0,0)
+    v2[0]=0
+    u2=(v2-Vec3D(0,1,0)*v2.norm()).normalized()
+    R=(Mat3D()-outer(u2)*2).transpose()
+    
+
+    v3=L1*M*R*Vec3D(0,1,0)
+    v3[0]=0.0
+    u3=(v3-Vec3D(0,1,0)*v3.norm()).normalized()
+    L2=Mat3D()-outer(u3)*2
+    return L2*L1, R
+
+ 
